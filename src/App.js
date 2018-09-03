fea1b97d4de42bedfc41879b7d88ea7a63bf3943
dev_ruban <kantharuban23@gmail.com>
2018-09-03 18:17:48 +0800

    Testing AWS

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Hi There! Docker react feature
+          Hi There! Docker react feature Testing AWS
         </p>
       </div>
     );
